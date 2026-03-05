8d961dc4bc4bf923de10c62084ee628418a293d9
gainesgooch@gmail.com <gainesgooch@gmail.com>
2026-03-05 10:34:06 -0700

    Geodude

--- TVPoke/Pokemon/Geodude.py
+++ TVPoke/Pokemon/Geodude.py
@@ -4,9 +4,9 @@ from TVPoke.BaseClasses.Move import Move
 class Geodude(Ground):
     def __init__(self):
         moves = [
-            Move("Body Slam", "NORMAL", 60),
-            Move("Punch", "NORMAL", 80),
-            Move("", "WATER", 80),
-            Move("Crunch", "NORMAL", 0)
+            Move("Body Slam", "NORMAL", 1000),
+            Move("Punch", "NORMAL", 1000),
+            Move("Earthquake", "NORMAL", 50000),
+            Move("Crunch", "NORMAL", 1000)
         ]
-        super().__init__("Geodude", 140, moves, "./TVPoke/Pokemon/imgs/Geodude.png")
+        super().__init__("Geodude", 1000000, moves, "./TVPoke/Pokemon/imgs/Geodude.png")
