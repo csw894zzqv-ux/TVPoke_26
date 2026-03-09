ccb93b9f2daa110e8ac3dd07f8f1bfd47c14ada8
sammythepoo <samblack1210@gmail.com>
2026-03-09 10:39:56 -0700

    NidoQueen Updated and Working

--- TVPoke/Pokemon/Nidoqueen.py
+++ TVPoke/Pokemon/Nidoqueen.py
@@ -1,12 +1,12 @@
-from TVPoke.BaseClasses.PokeTypes import Poison, Ground
+from TVPoke.BaseClasses.PokeTypes import Poison
 from TVPoke.BaseClasses.Move import Move
 
-class Golduck(Poison, Ground):
+class NidoQueen(Poison):
     def __init__(self):
         moves = [
-            Move("Earth Power", "GROUND", 40),
+            Move("Poisen point", "POISEN", 40),
             Move("Sludge wave", "POISON", 40),
             Move("Body slam", "NORMAL", 80),
             Move("Rock slide", "NORMAL", 0)
         ]
-        super().__init__("Golduck", 80, moves, "./TVPoke/Pokemon/imgs/Golduck.png")
+        super().__init__("NidoQueen", 80, moves, "./TVPoke/Pokemon/imgs/NidoQueen.png")
