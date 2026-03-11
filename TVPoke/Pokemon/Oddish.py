3277b18a04806f24604b63c8dc2ce757d5f6dc37
TaylorDeClay <soederbros00@icloud.com>
2026-03-11 11:17:25 -0700

    I added my moves and damage per move

--- TVPoke/Pokemon/Oddish.py
+++ TVPoke/Pokemon/Oddish.py
@@ -4,9 +4,10 @@ from TVPoke.BaseClasses.Move import Move
 class Oddish(Grass):
     def __init__(self):
         moves = [
-            Move(),
-            Move(),
-            Move(),
-            Move()
+            Move( "Petal Dance" , "GRASS" , 120),
+            Move( "Solar Beam" , "GRASS" , 120),
+            Move( "Sludge Bomb" , "POISON" , 90),
+            Move( "Energy Ball" , "GRASS" , 90),
+            Move( "Giga Drain" , "GRASS" , 75),
         ]
         super().__init__("Oddish", 90, moves, "./TVPoke/Pokemon/imgs/Oddish.png")
